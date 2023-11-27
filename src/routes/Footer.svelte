fcd7753e1979a568575b9862af2ed3a1df0d4700
Victor N <victorkjngo@gmail.com>
2023-11-27 15:29:45 -0800

    Replace unused footer elements

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,42 +2,23 @@
   <div class="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
     <!-- <img src="/img/logo-ct-dark.png" alt="logo-ct" class="w-10" /> -->
     <ul class="flex flex-wrap items-center gap-y-2 gap-x-8">
-      <li>
+      <!-- <li>
         <a
           href="#"
           class="block font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-pink-500 focus:text-pink-500"
         >
           About Us
         </a>
-      </li>
-      <li>
-        <a
-          href="#"
-          class="block font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-pink-500 focus:text-pink-500"
-        >
-          License
-        </a>
-      </li>
-      <li>
-        <a
-          href="#"
-          class="block font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-pink-500 focus:text-pink-500"
-        >
-          Contribute
-        </a>
-      </li>
-      <li>
-        <a
-          href="#"
-          class="block font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased transition-colors hover:text-pink-500 focus:text-pink-500"
-        >
-          Contact Us
-        </a>
-      </li>
+      </li> -->
     </ul>
   </div>
   <hr class="my-8 border-blue-gray-50" />
   <p class="block text-center font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased">
-    © 2023 Material Tailwind
-  </p>
+    <ol>
+      <li>
+        <a href="">Github</a>
+        <a href="">Twitter</a>
+        <a href="">Hashnode</a>
+        <a href="">LinkedIn</a>
+      </li>
 </footer>
